b9f5b719b14523ddcf5a8fab75169c84b691421f
Elián <elian.fabian.el.belaizi@gmail.com>
2022-03-12 23:57:05 +0100

    Update XMLReader.java
    The constructor now only receives the filename in order to avoid worrying about DocumentBuilder.
    
    There's a new function that returns a list of HasMaps.
    
    Some changes in variable names.

--- XMLReader.java
+++ XMLReader.java
@@ -1,31 +1,30 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
+package com.elian;
 
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class XMLReader
 {
-    String filename;
-    Document document;
+    public final String filename;
+    public final Document document;
 
     public static class Node
     {
-        public Element element;
-
-        //region Constructors
-        public Node(Element node)
-        {
-            this.element = node;
-        }
-
-        public Node() {}
-        //endregion
+        public Element content;
 
         //region Methods
         public String getString(String childName)
         {
-            return element.getElementsByTagName(childName).item(0).getTextContent();
+            return content.getElementsByTagName(childName).item(0).getTextContent();
         }
 
         public int getInt(String childName)
@@ -38,29 +37,69 @@ public class XMLReader
             return Float.parseFloat(getString(childName));
         }
 
-        public String getAttribute(String name)
+        public String getAttributeContent(String name)
         {
-            return element.getAttribute(name);
+            return content.getAttribute(name);
         }
         //endregion
     }
 
-    public XMLReader(Document document)
+    public XMLReader(String filename)
     {
+        Document document = null;
+
+        try
+        {
+            DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            document = docBuilder.parse(filename);
+        }
+        catch (ParserConfigurationException | IOException | SAXException e)
+        {
+            e.printStackTrace();
+        }
+
         this.document = document;
         this.filename = document.getDocumentURI();
     }
-    
+
     public void readNodesByName(String nodename, Consumer<Node> nodeConsumer)
     {
         Node node = new Node();
         NodeList nodeList = document.getElementsByTagName(nodename);
-        
+
         for (int i = 0; i < nodeList.getLength(); i++)
         {
-            node.element = (Element) nodeList.item(i);
+            node.content = (Element) nodeList.item(i);
 
             nodeConsumer.accept(node);
         }
+    }
+
+    public List<HashMap<String, String>> readNodesByName(String nodename)
+    {
+        List<HashMap<String, String>> objects = new ArrayList<>();
+
+        NodeList nodeList = document.getElementsByTagName(nodename);
+
+        for (int i = 0; i < nodeList.getLength(); i++)
+        {
+            NodeList children = (NodeList) nodeList.item(i);
+
+            HashMap<String, String> object = new HashMap<>();
+
+            for (int j = 0; j < children.getLength(); j++)
+            {
+                org.w3c.dom.Node child = children.item(j);
+
+                // Excludes the text nodes
+                if (Text.class.isAssignableFrom(child.getClass())) continue;
+
+                object.put(child.getNodeName(), child.getTextContent());
+            }
+
+            objects.add(object);
+        }
+
+        return objects;
     }
 }
